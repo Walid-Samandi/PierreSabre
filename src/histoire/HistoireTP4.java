a11991e9572cb189463a554317545ef6dc5c32d7
Walid <samandiwalid01@gmail.com>
2023-10-27 09:33:39 +0200

    création de la classe Commerçant

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,11 +1,5 @@
 package histoire;
 
 public class HistoireTP4 {
- //TODO
 	
-	public static void main(String[] args) {
-		//TODO
-	}
 }
-
-
---
+++ src/personnages/Commerçant.java
@@ -0,0 +1,33 @@
+package personnages;
+
+public class Commerçant extends Humain {
+
+	
+	public Commerçant(String nom, int argent) {
+		super(nom, "thé", argent);
+	}
+
+	private int seFaireExtorquer() {
+		perdreArgent(getArgent());
+		parler("J'ai tout perdu ! Le monde est trop injuste...");
+		return getArgent();
+	}
+	
+	
+	private void recevoir(int argent) {
+		gagnerArgent(argent);
+		parler(argent+ " sous ! Je te remercie généreux donateur !");
+	}
+	
+    public static void main(String[] args) {
+		Commerçant marco=new Commerçant("Marco",20);
+		System.out.println(marco);
+		marco.direBonjour();
+		marco.seFaireExtorquer();
+		System.out.println(marco);
+		marco.recevoir(15);
+		marco.boire();
+		System.out.println(marco);
+	}
+	
+}
